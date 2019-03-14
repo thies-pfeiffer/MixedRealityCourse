c9f02eeb9b8d48fa10f49623786703fb9f70a02f
Patrick Renner <prenner@techfak.uni-bielefeld.de>
2019-03-14 12:26:41 +0100

    Switched to ARController for Image marker handling.

--- IK_AugmentedReality/Assets/Scripts/ARController.cs
+++ IK_AugmentedReality/Assets/Scripts/ARController.cs
@@ -1,24 +1,4 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="AugmentedImageExampleController.cs" company="Google">
-//
-// Copyright 2018 Google Inc. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// </copyright>
-//-----------------------------------------------------------------------
-
-namespace GoogleARCore.Examples.AugmentedImage
+﻿namespace GoogleARCore.Examples.AugmentedImage
 {
     using System.Collections.Generic;
     using System.Runtime.InteropServices;
@@ -49,6 +29,9 @@ namespace GoogleARCore.Examples.AugmentedImage
 
         private Dictionary<int, AugmentedImageVisualizer> m_Visualizers
             = new Dictionary<int, AugmentedImageVisualizer>();
+
+        private Dictionary<int, Anchor> m_Anchors
+            = new Dictionary<int, Anchor>();
 
         private List<AugmentedImage> m_TempAugmentedImages = new List<AugmentedImage>();
 
@@ -106,26 +89,41 @@ namespace GoogleARCore.Examples.AugmentedImage
             {
                 AugmentedImageVisualizer visualizer = null;
                 m_Visualizers.TryGetValue(image.DatabaseIndex, out visualizer);
-                if (image.TrackingState == TrackingState.Tracking && visualizer == null)
+                if (image.TrackingState == TrackingState.Tracking)
                 {
-                    Debug.Log("Tracking " + image.DatabaseIndex);
-                    // Create an anchor to ensure that ARCore keeps tracking this augmented image.
-                    Anchor anchor = image.CreateAnchor(image.CenterPose);
-                    visualizer = (AugmentedImageVisualizer)Instantiate(AugmentedImageVisualizerPrefab, anchor.transform);
-                    visualizer.Image = image;
-                    m_Visualizers.Add(image.DatabaseIndex, visualizer);
-                    // Get image container and make it visible
-                    Transform imageContainer = imagesContainer.GetChild(image.DatabaseIndex);
-                    if (imageContainer != null)
+                    if (visualizer == null)
                     {
-                        imageContainer.position = anchor.transform.position;
-                        imageContainer.rotation = anchor.transform.rotation;
-                        imageContainer.gameObject.SetActive(true);
+                        Debug.Log("Tracking " + image.DatabaseIndex);
+                        // Create an anchor to ensure that ARCore keeps tracking this augmented image.
+                        Anchor anchor = image.CreateAnchor(image.CenterPose);
+                        m_Anchors.Add(image.DatabaseIndex, anchor);
+                        visualizer = (AugmentedImageVisualizer)Instantiate(AugmentedImageVisualizerPrefab, anchor.transform);
+                        visualizer.Image = image;
+                        m_Visualizers.Add(image.DatabaseIndex, visualizer);
+                        // Get image container and make it visible
+                        Transform imageContainer = imagesContainer.GetChild(image.DatabaseIndex);
+                        if (imageContainer != null)
+                        {
+                            imageContainer.position = anchor.transform.position;
+                            imageContainer.rotation = anchor.transform.rotation;
+                            imageContainer.gameObject.SetActive(true);
+                        }
+                    }
+                    else
+                    {
+                        // If the image is continuing tracking, update the transformation
+                        Transform imageContainer = imagesContainer.GetChild(image.DatabaseIndex);
+                        if (imageContainer != null)
+                        {
+                            imageContainer.position = m_Anchors[image.DatabaseIndex].transform.position;
+                            imageContainer.rotation = m_Anchors[image.DatabaseIndex].transform.rotation;
+                        }
                     }
                 }
                 else if (image.TrackingState == TrackingState.Stopped && visualizer != null)
                 {
                     m_Visualizers.Remove(image.DatabaseIndex);
+                    m_Anchors.Remove(image.DatabaseIndex);
                     Transform imageContainer = imagesContainer.GetChild(image.DatabaseIndex);
                     if (imageContainer != null)
                     {
